7f52577d1267c06d7f36af76a5002e6a3d03c29f
DhananjaiPS <143176852+DhananjaiPS@users.noreply.github.com>
2023-09-17 23:19:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const appointment-wrapper=document.querySelector('.appointment-wrapper');
-const loginlink=document.querySelector('.login-link')
-const registerlink=document.querySelector('.register-link')
+let appointment=document.getElementById('appointmentWrapper');
+let loginLink= document.getElementById('loginLink');
+let registerLink=document.getElementById('registerLink');
+let reg=document.getElementById("registerId");
 
-registerlink.addEventListener('click',()=>{
-    appointment-wrapper.classList.add('active');});
-loginlink.addEventListener('click',()=>{
-    appointment-wrapper.classList.remove('active');});
+loginLink.onclick=function(){
+    reg.style.height="0";
+}
