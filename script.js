3dbfa3227621eb908ff926f65b40a3660610f8bf
DhananjaiPS <143176852+DhananjaiPS@users.noreply.github.com>
2023-09-22 00:30:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let appointment=document.getElementById('appointmentWrapper');
-let loginLink= document.getElementById('loginLink');
-let registerLink=document.getElementById('registerLink');
-let reg=document.getElementById("registerId");
+// let appointment=document.getElementById('appointmentWrapper');
+// let loginLink= document.getElementById('loginLink');
+// let registerLink=document.getElementById('registerLink');
+// let reg=document.getElementById("registerId");
 
-loginLink.onclick=function(){
-    reg.style.height="0";
-}
+// loginLink.onclick=function(){
+//     reg.style.height="0";
+// };
+
+
+let scrollContainer=document.querySelector(".panel-3");
+let backBtn = document.getElementById("backbtn");
+let nextBtn = document.getElementById("nextbtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft +=evt.deltaY;
+
+});
+nextBtn.addEventListener("click" , ()=>{
+
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft -= 900;
+});
+
