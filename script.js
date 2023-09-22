608d08f1d1b3c67627dc25d97195e3c3373c8497
DhananjaiPS <143176852+DhananjaiPS@users.noreply.github.com>
2023-09-22 08:28:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,15 @@
 let scrollContainer=document.querySelector(".panel-3");
 let backBtn = document.getElementById("backbtn");
 let nextBtn = document.getElementById("nextbtn");
+let loader= document.getElementById("preloader");
+let subloader= document.getElementById("sub-preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none" ;
+});
+window.addEventListener("load", function(){
+    subloader.style.display = "none" ;
+});
 
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
